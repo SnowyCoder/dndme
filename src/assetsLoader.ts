aac0a66c4115a143314374215b90a577ff67b4f8
Rossi Lorenzo <snowycoder@gmail.com>
2022-12-10 16:57:47 +0100

    Remove usage of deprecated pixi function

--- src/assetsLoader.ts
+++ src/assetsLoader.ts
@@ -11,16 +11,16 @@ async function loadSpritesheet(loader: PIXI.Loader, name: string, imgUrl: string
         console.log("[Spritesheet] Loading: " + name);
         const imgElm = new Image();
         imgElm.crossOrigin = 'anonymous';
-        imgElm.onload = () => {
+        imgElm.onload = async () => {
             const tex = new PIXI.Texture(new BaseTexture(new ImageResource(imgElm)));
             const spritesheet = new PIXI.Spritesheet(tex, data);
-            loader.resources[name] = new (PIXI.LoaderResource as any)(name, "");
+            loader.resources[name] = new PIXI.LoaderResource(name, "");
             loader.resources[name].spritesheet = spritesheet;
-            spritesheet.parse(() => {
-                console.log("[Spritesheet] Loaded: " + name);
-                resolve(spritesheet);
-            });
+            await spritesheet.parse();
+            console.log("[Spritesheet] Loaded: " + name);
+            resolve(spritesheet);
         };
+        imgElm.onerror = () => reject("Error loading image " + name);
         imgElm.src = imgUrl; // When the listeners are set, we can finally start downloading the image.
     });
 }
